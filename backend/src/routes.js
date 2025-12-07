705f980958567f0c0a5f495f6b5198837ba1b1ce
Kailash Parihar <kailashparihar1999@gmail.com>
2025-12-08 01:47:17 +0530

    feat: Add initial backend API for routes, flight and schedule management,

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,18 @@ import express from "express";
 import authRoutes from "./routers/auth.routes.js";
 import userRoutes from "./routers/users.routes.js";
 
+import routeRoutes from "./routers/routes.routes.js";
+import flightRoutes from "./routers/flights.routes.js";
+import scheduleRoutes from "./routers/schedules.routes.js";
+
 const router = express.Router();
 
 // Registering API routes
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+
+router.use("/routes", routeRoutes);
+router.use("/flights", flightRoutes);
+router.use("/schedules", scheduleRoutes);
 
 export default router;
